cb34a4bfa86f91c8457fcd884e0f01f2b72eb533
Alex White <pixeljuggle@gmail.com>
2021-03-16 21:05:00 +0000

    Adding hunger and fitness prototype

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -10,7 +10,7 @@ describe('constructor', () => {
     expect(pet.name).toEqual('ChaCha');
   });
 
-  it('starts at the age of 0', () => {
+  it('initial age should be 0', () => {
     const pet = new Pet('ChaCha');
     expect(pet.age).toEqual(0);
   });
@@ -20,12 +20,52 @@ describe('constructor', () => {
     pet.growUp();
     expect(pet.age).toEqual(1);
   });
+
   it('grows up to 3', () => {
     const pet = new Pet('ChaCha');
     pet.growUp();
     pet.growUp();
     pet.growUp();
     expect(pet.age).toEqual(3);
+  });
+
+  it('initial hunger should be 0', () => {
+    const pet = new Pet('ChaCha');
+    expect(pet.hunger).toEqual(0);
+  });
+
+  it('increases the pets hunger by 5', () => {
+    const pet = new Pet('ChaCha');
+    pet.growUp();
+    expect(pet.hunger).toEqual(5);
+  });
+
+  it('increases the pets hunger to 20', () => {
+    const pet = new Pet('ChaCha');
+    pet.growUp();
+    pet.growUp();
+    pet.growUp();
+    pet.growUp();
+    expect(pet.hunger).toEqual(20);
+  });
+
+  it('initial fitness should be 10', () => {
+    const pet = new Pet('ChaCha');
+    expect(pet.fitness).toEqual(10);
+  });
+
+  it('decreases the pets fitness by 3', () => {
+    const pet = new Pet('ChaCha');
+    pet.growUp();
+    expect(pet.fitness).toEqual(7);
+  });
+
+  it('decreases the pets fitness to 1', () => {
+    const pet = new Pet('ChaCha');
+    pet.growUp();
+    pet.growUp();
+    pet.growUp();
+    expect(pet.fitness).toEqual(1);
   });
 
 });
